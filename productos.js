fed5b7517c77a7dd2a313e23ad8b6a2c6c36772f
marycrocheting26 <marycrocheting26@gmail.com>
2026-08-10 08:14:01 -0300

    Actualizaciones

--- productos.js
+++ productos.js
@@ -17,7 +17,9 @@ const productos = {
 
             "img/juego de baño marroncito.jpg",
 
-            "img/juego de baño negro completo.jpg"
+            "img/juego de baño negro completo.jpg",
+            "img/juego de baño rosita.jpg"
+
 
         ]
 
@@ -39,17 +41,26 @@ const productos = {
             "img/individual rosita.jpg",
             "img/individual con flores rojas alrededor.jpg",
             "img/individual blanco.jpg",
+            "img/individual rectang.jpg",
+            "img/individual blanco con posavasos.jpg",
+            "img/individual  verde con blanco.jpg",
+            "img/individual con bordados amarillos.jpg",
+            "img/individual forma de flor blanco.jpg",
+            "img/individual blanco con oro.jpg"
+
+
+
 
         ]
 
     },
 
-    "camino-mesa": {
+    "camino-centro-mesa": {
 
-        titulo: "Caminos de mesa",
+        titulo: "Caminos y centros de mesa",
 
         descripcion:
-        "Caminos de mesa tejidos a mano que aportan elegancia y un encanto artesanal a cualquier ambiente.",
+        "Caminos y centros de mesa tejidos a mano que aportan elegancia y un encanto artesanal a cualquier ambiente.",
 
         imagenes: [
 
@@ -57,7 +68,16 @@ const productos = {
 
             "img/camino de mesa floreado.jpg",
 
-            "img/camino de mesa naranja.jpg"
+            "img/camino de mesa naranja.jpg",
+            "img/camino de mesa blanco con flor roja.jpg",
+            "img/camino mesa flores celestes.jpg",
+            "img/camino mesa flores grandes.jpg",
+            "img/centro de mesa marroncito.jpg",
+            "img/centro de mesa con bordes oros.jpg"
+
+
+
+
 
         ]
 
@@ -82,6 +102,81 @@ const productos = {
             "img/bolso rojo y rosita.jpg",
             "img/bolso naranja y amarillo.jpg",
             "img/bolso pasteles.jpg"
+
+        ]
+
+    },
+
+    "Alfombras": {
+
+        titulo: "Alfombras",
+
+        descripcion:
+        "Alfombras que combinan texturas, formas y detalles únicos para darle calidez y personalidad a cada espacio del hogar.",
+        
+        imagenes: [
+
+            "img/alfombra redonda azul.jpg",
+            "img/alfombra redonda fuxia.jpg",
+            "img/alfombra gris y negro.jpg",
+            "img/alfombra blanco y negro.jpg",
+            "img/alfombra gris con flor rosa.jpg",
+            "img/alfombra gris con flor celeste.jpg",
+            "img/alfombra violeta.jpg",
+            "img/alfombra marron pastel.jpg",
+            "img/alfombra negro con pastel.jpg",
+            "img/alfombra larga rosa.jpg",
+            "img/alfombra bordo con negro.jpg",
+            "img/alfombra larga marrones oscuros.jpg",
+            "img/alfombra larga marron y flor.jpg",
+            "img/alfombra corta marron flor rosa.jpg",
+            "img/alfombra verdes.jpg",
+            "img/alfombra azul con negro.jpg",
+            "img/alfombras marrones claritos.jpg",
+            "img/alfombra roja y negra.jpg"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+        ]
+
+    },
+
+
+    "funda-garrafa-bidon": {
+
+        titulo: "Funda para garrafa y bidón de agua",
+
+        descripcion:
+        "Fundas tejidas a mano que aportan un detalle decorativo y cálido, ayudando a integrar la garrafa con el estilo de tu hogar.",
+
+        
+        imagenes: [
+
+            "img/funda garrafa flores blancas.jpg",
+            "img/garrafa marron con flor.png",
+            "img/garrafa roja con flor.jpg",
+            "img/funda agua flor amarilla.jpg"
+
+           
+
+
+
+
+            
 
         ]
 
